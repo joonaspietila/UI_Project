de03febf88de753b83bf4960240096ea56160044
joonaspietila <56837862+joonaspietila@users.noreply.github.com>
2021-01-24 20:57:31 +0200

    Update

---
+++ OpenBook.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class OpenBook : MonoBehaviour
+{
+
+    [SerializeField] Button openBtn;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip openBook;
+
+
+    private Vector3 rotationVector;
+    private bool isOpenCLicked;
+    private DateTime startTime;
+    private DateTime endTime;
+
+    void Start()
+    {
+        if (openBtn != null)
+            openBtn.onClick.AddListener(() => openBtn_Click());
+    }
+
+    
+    void Update()
+    {
+        if (isOpenCLicked)
+        {
+            transform.Rotate(rotationVector * Time.deltaTime);
+            endTime = DateTime.Now;
+
+            if ((endTime - startTime).TotalSeconds >= 1)
+            {
+                isOpenCLicked = false;
+            }
+
+        }
+    }
+
+    private void openBtn_Click()
+    {
+        isOpenCLicked = true;
+        startTime = DateTime.Now;
+
+
+        rotationVector = new Vector3(0, 180, 0);
+
+        PlaySound();
+    }
+
+    private void PlaySound()
+    {
+        if ((audioSource != null) && (openBook != null))
+        {
+            audioSource.PlayOneShot(openBook);
+        }
+    }
+}
